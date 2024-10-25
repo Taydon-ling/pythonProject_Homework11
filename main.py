cdc333583add9d11ada1b3430e9368217ee60c85
Taydon <2889837@student.dublinisc.com>
2024-10-25 15:35:06 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+l = int(input("What is the length of the rectangle?"))
+m = int(input("What is the width of the rectangle?"))
+
+# Use Length times by height to calculate the area
+print(l*m)
+
+print("This is the answer")
